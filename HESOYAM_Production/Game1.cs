44d624073e0bf901f71ab76dfa7e2408bec1ae80
gc <gc>
2016-03-31 09:24:04 +0200

    trochę testowych smieci

--- HESOYAM_Production/Game1.cs
+++ HESOYAM_Production/Game1.cs
@@ -14,6 +14,9 @@ namespace HESOYAM_Production
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
+		Model myModel;
+		float aspectRatio;
+
 
 		public Game1 ()
 		{
@@ -30,7 +33,7 @@ namespace HESOYAM_Production
 		protected override void Initialize ()
 		{
 			// TODO: Add your initialization logic here
-            
+
 			base.Initialize ();
 		}
 
@@ -43,7 +46,9 @@ namespace HESOYAM_Production
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch (GraphicsDevice);
 
-			//TODO: use this.Content to load your game content here 
+			//TODO: use this.Content to load your game content here
+			myModel = Content.Load<Model>("Cube");
+			aspectRatio = graphics.GraphicsDevice.Viewport.AspectRatio;
 		}
 
 		/// <summary>
@@ -59,11 +64,22 @@ namespace HESOYAM_Production
 			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState ().IsKeyDown (Keys.Escape))
 				Exit ();
 			#endif
-            
+
 			// TODO: Add your update logic here
-            
+			// Allows the game to exit
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == 
+				ButtonState.Pressed)
+				this.Exit();
+
+			modelRotation += (float)gameTime.ElapsedGameTime.TotalMilliseconds *
+				MathHelper.ToRadians(0.1f);
+
 			base.Update (gameTime);
 		}
+
+		float modelRotation = 0.0f;
+		Vector3 modelPosition = Vector3.Zero;
+		Vector3 cameraPosition = new Vector3(0.0f, 50.0f, 5000.0f);
 
 		/// <summary>
 		/// This is called when the game should draw itself.
@@ -72,9 +88,36 @@ namespace HESOYAM_Production
 		protected override void Draw (GameTime gameTime)
 		{
 			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
-            
+
 			//TODO: Add your drawing code here
-            
+
+			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// Copy any parent transforms.
+			Matrix[] transforms = new Matrix[myModel.Bones.Count];
+			myModel.CopyAbsoluteBoneTransformsTo(transforms);
+
+			// Draw the model. A model can have multiple meshes, so loop.
+			foreach (ModelMesh mesh in myModel.Meshes)
+			{
+				// This is where the mesh orientation is set, as well 
+				// as our camera and projection.
+				foreach (BasicEffect effect in mesh.Effects)
+				{
+					effect.EnableDefaultLighting();
+					effect.World = transforms[mesh.ParentBone.Index] * 
+						Matrix.CreateRotationY(modelRotation)
+						* Matrix.CreateTranslation(modelPosition);
+					effect.View = Matrix.CreateLookAt(cameraPosition, 
+						Vector3.Zero, Vector3.Up);
+					effect.Projection = Matrix.CreatePerspectiveFieldOfView(
+						MathHelper.ToRadians(45.0f), aspectRatio, 
+						1.0f, 10000.0f);
+				}
+				// Draw the mesh, using the effects set above.
+				mesh.Draw();
+			}
+
 			base.Draw (gameTime);
 		}
 	}
