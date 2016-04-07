4768792effddcf625a80ca81e6ee34bb167048b0
d0niek <damianglinkowski@gmail.com>
2016-04-07 09:44:44 +0200

    Zmiana nazwy głównej - Engine

---
+++ HESOYAM_Production/Engine.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+namespace HESOYAM_Production
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Engine : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Model myModel;
+        float aspectRatio;
+
+
+        public Engine ()
+        {
+            graphics = new GraphicsDeviceManager (this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize ()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize ();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent ()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch (GraphicsDevice);
+
+            //TODO: use this.Content to load your game content here
+            myModel = Content.Load<Model> ("Cube");
+            aspectRatio = graphics.GraphicsDevice.Viewport.AspectRatio;
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update (GameTime gameTime)
+        {
+            // For Mobile devices, this logic will close the Game when the Back button is pressed
+            // Exit() is obsolete on iOS
+            #if !__IOS__ &&  !__TVOS__
+            if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState ().IsKeyDown (Keys.Escape))
+                Exit ();
+            #endif
+
+            // TODO: Add your update logic here
+            // Allows the game to exit
+            if (GamePad.GetState (PlayerIndex.One).Buttons.Back ==
+            ButtonState.Pressed)
+                this.Exit ();
+
+            modelRotation += (float)gameTime.ElapsedGameTime.TotalMilliseconds *
+            MathHelper.ToRadians (0.1f);
+
+            base.Update (gameTime);
+        }
+
+        float modelRotation = 0.0f;
+        Vector3 modelPosition = Vector3.Zero;
+        Vector3 cameraPosition = new Vector3 (0.0f, 50.0f, 5000.0f);
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw (GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+
+            //TODO: Add your drawing code here
+
+            graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+
+            // Copy any parent transforms.
+            Matrix[] transforms = new Matrix[myModel.Bones.Count];
+            myModel.CopyAbsoluteBoneTransformsTo (transforms);
+
+            // Draw the model. A model can have multiple meshes, so loop.
+            foreach (ModelMesh mesh in myModel.Meshes) {
+                // This is where the mesh orientation is set, as well 
+                // as our camera and projection.
+                foreach (BasicEffect effect in mesh.Effects) {
+                    effect.EnableDefaultLighting ();
+                    effect.World = transforms [
+                        mesh.ParentBone.Index] *
+                    Matrix.CreateRotationY (modelRotation)
+                    * Matrix.CreateTranslation (modelPosition);
+                    effect.View = Matrix.CreateLookAt (cameraPosition, 
+                                       Vector3.Zero, Vector3.Up);
+                    effect.Projection = Matrix.CreatePerspectiveFieldOfView (
+                        MathHelper.ToRadians (45.0f), aspectRatio, 
+                        1.0f, 10000.0f);
+                }
+                // Draw the mesh, using the effects set above.
+                mesh.Draw ();
+            }
+
+            base.Draw (gameTime);
+        }
+    }
+}
+
--- HESOYAM_Production/Program.cs
+++ HESOYAM_Production/Program.cs
@@ -23,11 +23,11 @@ namespace HESOYAM_Production
 	static class Program
     #endif
     {
-		private static Game1 game;
+		private static Engine game;
 
 		internal static void RunGame ()
 		{
-			game = new Game1 ();
+			game = new Engine ();
 			game.Run ();
 			#if !__IOS__  && !__TVOS__
 			game.Dispose ();
