811a6f453497156e4f7a19f896f611f09f5cd6ba
d0niek <damianglinkowski@gmail.com>
2016-04-12 12:51:10 +0200

    Obiekt player

---
+++ HESOYAM_Production/App/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace App
+{
+
+    public class Player : GameObject
+    {
+        public Player(
+            Game game,
+            Vector3 position = default(Vector3),
+            Vector3 rotaion = default(Vector3)
+        ) : base(game, position, rotaion)
+        {
+        }
+
+        public void update()
+        {
+            
+        }
+    }
+}
+
