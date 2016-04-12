1a7f862e8c5a6b8569cdbe48eecab188561646bd
d0niek <damianglinkowski@gmail.com>
2016-04-12 12:56:17 +0200

    Wyczyszczenie obiektu kamera

--- HESOYAM_Production/App/Camera.cs
+++ HESOYAM_Production/App/Camera.cs
@@ -6,57 +6,23 @@ namespace App
 
     public class Camera : GameObject
     {
-        // Direction the camera points without rotation.
-        private Vector3 cameraReference = new Vector3(0, 0, 1);
-
-        public Vector3 TargetOffset { get; set; }
-
         public Matrix ViewMatrix { get; set; }
 
         public Matrix ProjectionMatrix { get; set; }
 
-        public Camera(Game game, Vector3 p = default(Vector3), Vector3 r = default(Vector3)) : base(game, p, r)
+        public Camera(
+            Game game,
+            Vector3 position = default(Vector3),
+            Vector3 rotaion = default(Vector3)
+        ) : base(game, position, rotaion)
         {
             this.ViewMatrix = Matrix.Identity;
             this.ProjectionMatrix = Matrix.Identity;
         }
 
-        public void update(InputState input, GameTime gameTime, float aspectRatio)
+        public void update(float aspectRatio)
         {
-            if (input.LastMouseState.Position != Point.Zero) {
-                Point mousePosition = input.CurrentMouseState.Position - input.LastMouseState.Position;
-
-                this.Rotate(
-                    -GameTimeFloat(gameTime) * MathHelper.ToRadians(mousePosition.Y) / 40.0f,
-                    -GameTimeFloat(gameTime) * MathHelper.ToRadians(mousePosition.X) / 40.0f,
-                    0
-                );
-            }
-
-            Matrix rotationMatrixY = Matrix.CreateRotationY(this.rotation.Y);
-            Matrix rotationMatrixX = Matrix.CreateRotationX(this.rotation.X);
-            Vector3 transformedReference = Vector3.Transform(this.cameraReference, rotationMatrixX * rotationMatrixY);
-
-            PlayerIndex pi;
-            Vector3 v = Vector3.Zero;
-
-            if (input.IsKeyPressed(Keys.W, PlayerIndex.One, out pi)) {
-                v.Z = -10;
-                v = Vector3.Transform(v, rotationMatrixY);
-            } else if (input.IsKeyPressed(Keys.S, PlayerIndex.One, out pi)) {
-                v.Z = 10;
-                v = Vector3.Transform(v, rotationMatrixY);
-            } else if (input.IsKeyPressed(Keys.A, PlayerIndex.One, out pi)) {
-                v.X = -10;
-                v = Vector3.Transform(v, rotationMatrixY);
-            } else if (input.IsKeyPressed(Keys.D, PlayerIndex.One, out pi)) {
-                v.X = 10;
-                v = Vector3.Transform(v, rotationMatrixY);
-            }
-
-            this.Move(v.X, 0, v.Z);
-
-            Vector3 cameraLookAt = this.position + transformedReference;
+            Vector3 cameraLookAt = Vector3.Zero;
 
             this.ViewMatrix = Matrix.CreateLookAt(this.position, cameraLookAt, Vector3.Up);
             this.ProjectionMatrix = Matrix.CreatePerspectiveFieldOfView(
@@ -65,11 +31,6 @@ namespace App
                 1.0f,
                 10000.0f
             );
-        }
-
-        private float GameTimeFloat(GameTime gameTime)
-        {
-            return (float) gameTime.ElapsedGameTime.TotalMilliseconds;
         }
     }
 }
