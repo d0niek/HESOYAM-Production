41212dcd4dd36e7ed39c4a43449e3b5819d25746
d0niek <damianglinkowski@gmail.com>
2016-04-13 21:05:20 +0200

    Podążanie za kamerą

--- HESOYAM_Production/App/Player.cs
+++ HESOYAM_Production/App/Player.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Input;
 using System;
 using App.Collisions;
+using HESOYAM_Production;
 
 namespace App
 {
@@ -9,7 +10,7 @@ namespace App
     public class Player : GameObject
     {
         public Player(
-            Game game,
+            Engine game,
             string name,
             Vector3 position = default(Vector3),
             Vector3 rotaion = default(Vector3)
@@ -28,56 +29,72 @@ namespace App
             Vector3 vector = Vector3.Zero;
 
             if (input.IsKeyPressed(Keys.W, PlayerIndex.One, out playerIndex)) {
-                vector.Z = -10;
-            }
-
-            if (input.IsKeyPressed(Keys.S, PlayerIndex.One, out playerIndex)) {
                 vector.Z = 10;
             }
 
+            if (input.IsKeyPressed(Keys.S, PlayerIndex.One, out playerIndex)) {
+                vector.Z = -10;
+            }
+
             if (input.IsKeyPressed(Keys.A, PlayerIndex.One, out playerIndex)) {
-                vector.X = -10;
+                vector.X = 10;
             }
 
             if (input.IsKeyPressed(Keys.D, PlayerIndex.One, out playerIndex)) {
-                vector.X = 10;
+                vector.X = -10;
             }
 
             vector = Vector3.Transform(vector, rotationMatrixY);
             this.Move(vector.X, 0, vector.Z);
         }
 
-        public void Rotate(float x, float y, float z)
+        public new void Rotate(float x, float y, float z)
         {
-            Vector3 delta = new Vector3(x, y, z);
-            rotation = Vector3.Add(delta, rotation);
+            Vector3 newRotation = new Vector3(x, y, z);
 
-            foreach (IGameElement child in children.Values) {
-                if (!(child is Camera)) {
-                    child.Rotate(x, y, z);
+            if (this.rotation != newRotation) {
+                rotation = newRotation;
+
+                foreach (IGameElement child in children.Values) {
+                    if (!(child is Camera)) {
+                        child.Rotate(x, y, z);
+                    }
                 }
-            }
 
-            foreach (Collider collider in colliders) {
-                collider.RotateAroundParent(x, y, z);
+                foreach (Collider collider in colliders) {
+                    collider.RotateAroundParent(x, y, z);
+                }
             }
         }
 
         private float getAngleFromMouse(InputState input)
         {
-            float angle = 0.0f;
+            float angle = this.rotation.Y;
             Vector2 windowSize = new Vector2(
                                      this.Game.GraphicsDevice.Viewport.Width,
                                      this.Game.GraphicsDevice.Viewport.Height
                                  );
             Vector2 mousePos = new Vector2(input.CurrentMouseState.X, input.CurrentMouseState.Y);
 
-            if (mousePos.X >= 0 && mousePos.Y >= 0 && mousePos.X <= windowSize.X && mousePos.Y <= windowSize.Y) {
-                Vector2 direction = mousePos - (windowSize / 2);
-                angle = (float) (Math.Atan2(direction.Y, direction.X));
+            if (this.isMouseInGameWindow(mousePos, windowSize)) {
+                mousePos.X -= windowSize.X / 2;
+                mousePos.Y -= windowSize.Y / 2;
+
+                angle = (float) (Math.Atan2(mousePos.X, mousePos.Y));
+                angle += (float) (Math.Atan2(this.game.camera.startPosition.X, this.game.camera.startPosition.Z));
             }
 
             return angle;
+        }
+
+        private bool isMouseInGameWindow(Vector2 mouse, Vector2 window)
+        {
+            bool leftBorder = mouse.X >= 0;
+            bool topBorder = mouse.Y >= 0;
+            bool rightBorder = mouse.X <= window.X;
+            bool bottomBorder = mouse.Y <= window.Y;
+
+            return leftBorder && topBorder && rightBorder && bottomBorder;
         }
     }
 }
