6c247fafca50f632f395590f75e42c8b651804eb
d0niek <damianglinkowski@gmail.com>
2016-05-09 16:27:58 +0200

    Poprawienie czytelności kodu

--- HESOYAM_Production/App/Scene.cs
+++ HESOYAM_Production/App/Scene.cs
@@ -15,35 +15,48 @@ namespace App
             Bitmap bmp = (Bitmap) Image.FromFile(bitmapPath);
             this.AddChild(new GameObject(game, "Walls"));
 
-            this.buildWalls(bmp, wall);
+            this.buildMap(bmp, wall);
             this.buildFloor(bmp.Width, bmp.Height, wall);
 
             this.AddChildrenToGame(true);
         }
 
-        private void buildWalls(Bitmap bmp, Model model)
+        private void buildMap(Bitmap bmp, Model model)
         {
             for (int i = 0; i < bmp.Height; i++) {
                 for (int j = 0; j < bmp.Width; j++) {
                     System.Drawing.Color color = bmp.GetPixel(i, j);
 
-                    if (color.R == 0 && color.G == 0 && color.B == 0) {
-                        GameObject wall = new GameObject(
-                                              game,
-                                              "Wall_" + j + "x" + i,
-                                              model,
-                                              new Vector3(i * Shift, 0f, j * Shift)
-                                          );
-                        this.children["Walls"].AddChild(wall);
-                    }
+                    this.buildMapObject(color, model, i, j);
                 }
             }
         }
 
+        private void buildMapObject(System.Drawing.Color color, Model model, int x, int y)
+        {
+            if (color.R == 0 && color.G == 0 && color.B == 0) {
+                this.buildWall(model, x, y);
+            }
+        }
+
+        private void buildWall(Model model, int x, int y)
+        {
+            GameObject wall = new GameObject(
+                                  game,
+                                  "Wall_" + x + "x" + y,
+                                  model,
+                                  new Vector3(x * Shift, 0f, y * Shift)
+                              );
+
+            this.children["Walls"].AddChild(wall);
+        }
+
         private void buildFloor(int Width, int Height, Model model)
         {
-            float positionX = Height * Shift / 2;
-            float positionZ = Width * Shift / 2;
+            int modelWidth = 50;
+
+            float positionX = Height * Shift / 2 - modelWidth;
+            float positionZ = Width * Shift / 2 - modelWidth;
 
             GameObject floor = new GameObject(
                                    this.game,
