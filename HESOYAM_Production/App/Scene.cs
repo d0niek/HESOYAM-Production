50843538c23a6749a49f0e6d7067b081381c476e
d0niek <damianglinkowski@gmail.com>
2016-05-09 16:40:47 +0200

    Zmiana nazwy

--- HESOYAM_Production/App/Scene.cs
+++ HESOYAM_Production/App/Scene.cs
@@ -8,21 +8,22 @@ namespace App
 
     public class Scene: GameObject
     {
-        private const float Shift = 100;
+        private const float WallShift = 100;
 
         public Scene(Engine game, string name, string bitmapPath, Model wall) : base(game, name)
         {
             Bitmap bmp = (Bitmap) Image.FromFile(bitmapPath);
-            this.AddChild(new GameObject(game, "Walls"));
 
             this.buildMap(bmp, wall);
-            this.buildFloor(bmp.Width, bmp.Height, wall);
+            this.drawFloor(bmp.Width, bmp.Height, wall);
 
             this.AddChildrenToGame(true);
         }
 
         private void buildMap(Bitmap bmp, Model model)
         {
+            this.AddChild(new GameObject(game, "Walls"));
+
             for (int i = 0; i < bmp.Width; i++) {
                 for (int j = 0; j < bmp.Height; j++) {
                     System.Drawing.Color color = bmp.GetPixel(i, j);
@@ -45,18 +46,18 @@ namespace App
                                   game,
                                   "Wall_" + x + "x" + y,
                                   model,
-                                  new Vector3(x * Shift, 0f, y * Shift)
+                                  new Vector3(x * WallShift, 0f, y * WallShift)
                               );
 
             this.children["Walls"].AddChild(wall);
         }
 
-        private void buildFloor(int Width, int Height, Model model)
+        private void drawFloor(int Width, int Height, Model model)
         {
             int modelWidth = 50;
 
-            float positionX = Width * Shift / 2 - modelWidth;
-            float positionZ = Height * Shift / 2 - modelWidth;
+            float positionX = Width * WallShift / 2 - modelWidth;
+            float positionZ = Height * WallShift / 2 - modelWidth;
 
             GameObject floor = new GameObject(
                                    this.game,
