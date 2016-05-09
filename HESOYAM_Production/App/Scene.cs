db0015882187f6f5901a5caaca6ea4692828650b
d0niek <damianglinkowski@gmail.com>
2016-05-09 16:09:56 +0200

    Rysowanie podłogi

--- HESOYAM_Production/App/Scene.cs
+++ HESOYAM_Production/App/Scene.cs
@@ -8,24 +8,17 @@ namespace App
 
     public class Scene: GameObject
     {
+        private const float Shift = 100;
+
         public Scene(Engine game, string name, string bitmapPath, Model wall) : base(game, name)
         {
             Bitmap bmp = (Bitmap) Image.FromFile(bitmapPath);
             this.AddChild(new GameObject(game, "Walls"));
 
             this.buildWalls(bmp, wall);
+            this.buildFloor(bmp.Width, bmp.Height, wall);
 
-            GameObject floor = new GameObject(
-                                   game,
-                                   "Floor",
-                                   wall,
-                                   new Vector3((float) bmp.Height * 100f, -400f, (float) bmp.Width * 100f),
-                                   Vector3.Zero,
-                                   new Vector3((float) bmp.Height, 0.1f, (float) bmp.Width)
-                               );
-
-            this.AddChild(floor);
-            this.children["Walls"].AddChildrenToGame(true);
+            this.AddChildrenToGame(true);
         }
 
         private void buildWalls(Bitmap bmp, Model model)
@@ -37,14 +30,31 @@ namespace App
                     if (color.R == 0 && color.G == 0 && color.B == 0) {
                         GameObject wall = new GameObject(
                                               game,
-                                              "Wall" + j + "x" + i,
+                                              "Wall_" + j + "x" + i,
                                               model,
-                                              new Vector3(i * 100f, 0f, j * 100f)
+                                              new Vector3(i * Shift, 0f, j * Shift)
                                           );
                         this.children["Walls"].AddChild(wall);
                     }
                 }
             }
+        }
+
+        private void buildFloor(int Width, int Height, Model model)
+        {
+            float positionX = Height * Shift / 2;
+            float positionZ = Width * Shift / 2;
+
+            GameObject floor = new GameObject(
+                                   this.game,
+                                   "Floor",
+                                   model,
+                                   new Vector3(positionX, 0f, positionZ),
+                                   Vector3.Zero,
+                                   new Vector3((float) Height, 0f, (float) Width)
+                               );
+
+            this.AddChild(floor);
         }
     }
 }
