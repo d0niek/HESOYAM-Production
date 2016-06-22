37a81a9606e610df92ffae42475424403f53a21d
0h41 <perlikowski.marcin@gmail.com>
2016-06-22 22:47:51 +0200

    projectile

---
+++ HESOYAM_Production/App/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using HESOYAM_Production;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace App
+{
+
+    public class Projectile : GameObject
+    {
+        public Vector3 direction;
+        public float speed;
+
+        public Projectile(
+            Engine game,
+            string name,
+            Model model,
+            Vector3 position = default(Vector3),
+            Vector3 rotation = default(Vector3),
+            Vector3? scale = null
+        ) : base(game, name, model, position, rotation, scale)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            DrawModel(model);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
